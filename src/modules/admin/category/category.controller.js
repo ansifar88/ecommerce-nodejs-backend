3fdb1955602a3afd10afb9e107a99e72c03a3766
ansifar88 <ansifar88@gmail.com>
2025-09-21 12:34:39 +0530

    category module - part:2

--- src/modules/admin/category/category.controller.js
+++ src/modules/admin/category/category.controller.js
@@ -1,15 +1,19 @@
 import { BadRequestError } from "../../../utils/errors.js";
 import { successResponse } from "../../../utils/response.js";
-import { getCategoryByName, insertCategory } from "./category.service.js";
+import { getCategory, getCategoryByName, insertCategory } from "./category.service.js";
 
 export const getCategories = async (req, res, next) => {
   try {
-    const category = getAllCategory()
-    return successResponse(res, 200, "u have reached getcat contr",category);
+    const query = {...req.query}
+    
+    const category = await getCategory(query);
+    
+    return successResponse(res, 200, "Success", category);
   } catch (error) {
     next(error);
   }
 };
+
 export const createCategories = async (req, res, next) => {
   try {
     const isExists = await getCategoryByName(req.body.name);
--- src/modules/admin/category/category.service.js
+++ src/modules/admin/category/category.service.js
@@ -1,3 +1,4 @@
+import { getPagination } from "../../../utils/paginator.js";
 import Category from "./category.model.js";
 
 export const getCategoryByName = async (name) => {
@@ -6,7 +7,49 @@ export const getCategoryByName = async (name) => {
 };
 
 export const insertCategory = async (data) => {
-
   const cat = await Category.create(data);
   return cat;
+};
+
+export const getCategory = async (query) => {
+  const { page, limit, skip } = getPagination(query);
+
+  const filter = {};
+
+  if (query.search) {
+    filter.name = { $regex: "^" + query.search, $options: "i" };
+  }
+  if (query.status !== undefined && query.status !== "all") {
+    filter.isActive = query.status === "active" ? true : false;
+  }
+  let sort = { createdAt: -1 }; // default sort
+  if (query.sortBy) {
+    sort = {
+      [query.sortBy]: query.sortOrder === "asc" ? 1 : -1,
+    };
+  }
+  const [categories, total] = await Promise.all([
+    Category.find(filter)
+      .skip(skip)
+      .limit(limit)
+      .sort(sort)
+      .populate("createdBy", "username")
+      .select("-__v")
+      .lean(),
+    Category.countDocuments(filter),
+  ]);
+  const result = categories.map((cat) => ({
+    ...cat,
+    createdBy: cat.createdBy?.username || null,
+    isActive: (cat.isActive = true ? "active" : "inactive"),
+  }));
+  return {
+    result,
+    pagination: {
+      page,
+      limit,
+      total,
+      totalPages: Math.ceil(total / limit),
+    },
+  };
 };
